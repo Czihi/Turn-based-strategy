959b9460c7918d168b8a3ed72fcf25170f29f24a
Czihi <boslak@student.agh.edu.pl>
2020-08-31 21:45:29 +0200

    Updates

---
+++ precache-manifest.6ab3d66709fac7d71a481cad21067da1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "76de33d50b543973252c85066fc093e5",
+    "url": "/Turn-based-strategy/static/media/House.76de33d5.png"
+  },
+  {
+    "revision": "8f5df3d31995a7fecaa4721d391d71bb",
+    "url": "/Turn-based-strategy/static/media/Farm.8f5df3d3.png"
+  },
+  {
+    "revision": "f95181ae8d76b689bbe6cddfad657436",
+    "url": "/Turn-based-strategy/static/media/Board.f95181ae.png"
+  },
+  {
+    "revision": "aa0b7b81c15731df8739",
+    "url": "/Turn-based-strategy/static/js/runtime~main.9c267862.js"
+  },
+  {
+    "revision": "caa5d909cbe8ce4bbc06",
+    "url": "/Turn-based-strategy/static/js/main.74cde240.chunk.js"
+  },
+  {
+    "revision": "a0565a25a57de26b005c",
+    "url": "/Turn-based-strategy/static/js/2.4676ba8f.chunk.js"
+  },
+  {
+    "revision": "caa5d909cbe8ce4bbc06",
+    "url": "/Turn-based-strategy/static/css/main.21930ff7.chunk.css"
+  },
+  {
+    "revision": "a0565a25a57de26b005c",
+    "url": "/Turn-based-strategy/static/css/2.11974964.chunk.css"
+  },
+  {
+    "revision": "5ad172fbda72f918ac3a2ec6b6616999",
+    "url": "/Turn-based-strategy/index.html"
+  }
+];
