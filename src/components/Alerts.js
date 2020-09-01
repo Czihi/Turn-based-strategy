49927fde9b8420f463f6e12801eb8dc17b620432
Czihi <boslak@student.agh.edu.pl>
2020-09-01 13:20:46 +0200

    Update README

--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -1,12 +1,13 @@
 import React from "react";
+import wood from "../images/Wood.png";
 
 const Alerts = () => {
     return (
         <div>
-            <div id="summary" className="daySummary">
+            <div id="summary" className="daySummary" style={{backgroundImage: `url(${wood})`}}>
                 Zakończono dzień
             </div>
-            <div id="resources" className="noResources">
+            <div id="resources" className="noResources" style={{backgroundImage: `url(${wood})`}}>
                 Brak środków na wybudowanie:
             </div>
         </div>
